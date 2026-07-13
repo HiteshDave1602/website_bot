ad31f1f2ff29bfe16d9017f8c7e5b4e13fe6237e
vrattiksKamya <vrattiks.intern2@gmail.com>
2026-07-13 18:18:59 +0530

    ui changes as per website appearance

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,20 +4,22 @@ import Home from './pages/Home';
 import ChatPage from './pages/ChatPage';
 import './App.css';
 
-function ChatRoute() {
-  return <ChatPage />;
+function HomeRoute() {
+  return (
+    <>
+      <Navbar />
+      <Home />
+    </>
+  );
 }
 
 export default function App() {
   return (
-    <>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/:website_id/chat" element={<ChatRoute />} />
-        <Route path="/bot/:website_id" element={<ChatRoute />} />
-        <Route path="/chat" element={<ChatRoute />} />
-      </Routes>
-    </>
+    <Routes>
+      <Route path="/" element={<HomeRoute />} />
+      <Route path="/:website_id/chat" element={<ChatPage />} />
+      <Route path="/bot/:website_id" element={<ChatPage />} />
+      <Route path="/chat" element={<ChatPage />} />
+    </Routes>
   );
 }
--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,9 +1,6 @@
-import { Link, useLocation } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 export default function Navbar() {
-  const location = useLocation();
-  const isHome = location.pathname === '/';
-
   return (
     <nav className="navbar">
       <div className="navbar-inner">
@@ -26,18 +23,6 @@ export default function Navbar() {
           </svg>
           <span className="brand-text">SiteBot</span>
         </Link>
-
-        <div className="navbar-links">
-          {!isHome && (
-            <Link to="/" className="nav-link">
-              <svg width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
-                <path d="M3 9l9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z" />
-                <polyline points="9 22 9 12 15 12 15 22" />
-              </svg>
-              Home
-            </Link>
-          )}
-        </div>
       </div>
     </nav>
   );
